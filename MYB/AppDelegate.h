386fba9172d588e407b21995f3447b5e0822e9fc
吴东 <wudong@xrdemacbook-pro.lan>
2016-08-05 11:14:12 +0800

    Initial Commit

---
+++ MYB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYB
+//
+//  Created by XR on 16/8/5.
+//  Copyright © 2016年 wudong.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYB
+//
+//  Created by XR on 16/8/5.
+//  Copyright © 2016年 wudong.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
